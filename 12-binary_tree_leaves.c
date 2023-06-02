b69ddd99ecd6efc183809d18820607ef3a73a7a6
blackoaz <pauloakello56@gmail.com>
2023-06-02 15:50:09 +0300

    updates

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -4,17 +4,6 @@
  * @tree: tree to be traversed
  * Return: returns the number of leaves
  */
-/*size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	if (tree->right == NULL && tree->left == NULL)
-		return (1);
-	size_t left_side = binary_tree_leaves(tree->left);
-	size_t right_side = binary_tree_leaves(tree->right);
-
-	return (left_side + right_side);
-}*/
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaves = 0;
