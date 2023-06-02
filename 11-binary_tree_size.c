ef323fbe41632d03c68622031d47f1b1e1af6ac5
blackoaz <pauloakello56@gmail.com>
2023-06-02 15:26:53 +0300

    updates

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,7 +8,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	
 	size_t left_side = binary_tree_size(tree->left);
 	size_t right_side = binary_tree_size(tree->right);
 
