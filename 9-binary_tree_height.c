6f43d532c9962d664d844e840ba7ec79fa6710e2
blackoaz <pauloakello56@gmail.com>
2023-06-02 14:40:02 +0300

    updates to height function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,11 +6,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	if (tree)
+	{
+		size_t left_side = 0, right_side = 0;
 
-	size_t left_subTree = binary_tree_height(tree->left);
-	size_t right_subTree = binary_tree_height(tree->right);
-
-	return 1 + (left_subTree > right_subTree ? left_subTree : right_subTree);
+		left_side = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right_side = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((left_side > right_side) ? left_side : right_side);
+	}
+	return (0);
 }
